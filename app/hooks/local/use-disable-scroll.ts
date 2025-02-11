01fc33807fd5661f87ba7dcca719548fac18a7fb
Shivam Singh <roxshivamsingh@gmail.com>
2025-02-11 22:45:21 +0530

    ADD: Common > module - previous app;

---
+++ app/hooks/local/use-disable-scroll.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export function useDisableScroll(disable: boolean) {
+  const [isDisabled, setIsDisabled] = useState(disable);
+
+  useEffect(() => {
+    setIsDisabled(disable);
+  }, [disable]);
+
+  useEffect(() => {
+    const previous = document.body.style.overflow;
+    document.body.style.overflow = isDisabled ? 'hidden' : previous;
+    return () => {
+      document.body.style.overflow = previous; // Cleanup
+    };
+  }, [isDisabled]);
+}
---
+++ app/hooks/network/action/index.ts
@@ -0,0 +1,2 @@
+export * from './use-firebase-auth-action';
+export * from './use-firebase-storage-action';
