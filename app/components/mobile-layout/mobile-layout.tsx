3c9ebbbe867a480736fc70d384b84644b76ae6da
Akshay Kumar <akshay.official.dev@gmail.com>
2025-02-14 13:23:53 +0530

    Added video on home page

--- app/components/mobile-layout/mobile-layout.tsx
+++ app/components/mobile-layout/mobile-layout.tsx
@@ -2,10 +2,11 @@ import { ReactNode } from 'react';
 // =========================================================
 
 import { MobileNavBar } from '~/components';
+
 export function MobileLayout({ children }: TProps) {
   return (
     <div className='min-h-screen bg-background'>
-      <div className='fixed top-0 left-0 right-0 h-7 bg-white flex items-center justify-center text-xs text-neutral-900 z-50'>
+      <div className='fixed top-0 left-0 right-0 h-8 bg-white flex items-center justify-center text-sm text-neutral-900 z-50'>
         magppie.com
       </div>
       <div className='pt-7'>{children}</div>
