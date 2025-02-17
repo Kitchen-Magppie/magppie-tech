cb56a6b5449b474be012e0de71848d8c1e7e00ab
Shivam Singh <roxshivamsingh@gmail.com>
2025-02-17 23:03:50 +0530

    FIX: Minor;

--- app/components/chat-coming-soon-dialog/chat-coming-soon-dialog.tsx
+++ app/components/chat-coming-soon-dialog/chat-coming-soon-dialog.tsx
@@ -13,11 +13,13 @@ export function ChatComingSoonDialog(props: TProps) {
   return (
     <Dialog>
       <DialogTrigger asChild>{props.children}</DialogTrigger>
-      <DialogContent className='w-[90%] h-[90vh] rounded-lg'>
+      <DialogContent className='w-[90%] h-[80vh] rounded-lg'>
         <DialogHeader>
-          <DialogTitle className='text-4xl'>Chat</DialogTitle>
+          <DialogTitle className='text-3xl text-left x-font-exo-500 '>
+            Chat
+          </DialogTitle>
           <DialogDescription className='h-full flex align-middle justify-center'>
-            <div className='x-font-exo-500 text-3xl my-auto'>Coming soon..</div>
+            <div className='x-font-exo-500 text-2xl my-auto'>Coming soon..</div>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
