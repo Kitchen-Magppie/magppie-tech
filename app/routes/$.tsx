d3cee6a2c6348a5e523498f413883fe2be3d1436
Shivam Singh <roxshivamsingh@gmail.com>
2025-02-17 23:48:00 +0530

    ADD: Not found;

---
+++ app/routes/$.tsx
@@ -0,0 +1,20 @@
+import { Button } from '~/components/ui/button';
+
+export default function NotFound() {
+  return (
+    <div className='flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16'>
+      <div className='w-full space-y-6 text-center'>
+        <div className='text-4xl ' style={{ fontWeight: 700 }}>
+          MAGPPIE
+        </div>
+        <div className='space-y-3'>
+          <h1 className='text-4xl font-bold tracking-tighter sm:text-5xl animate-bounce'>
+            404
+          </h1>
+          <p className='text-gray-500'>Page not found!</p>
+        </div>
+        <Button className='x-font-exo-500'>Return to website</Button>
+      </div>
+    </div>
+  );
+}
