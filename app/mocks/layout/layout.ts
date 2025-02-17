a7244d1aaa7d154f19c75e8c78c2c159461084ff
Shivam Singh <roxshivamsingh@gmail.com>
2025-02-17 15:18:17 +0530

    FIX: Icon animation;

--- app/mocks/layout/layout.ts
+++ app/mocks/layout/layout.ts
@@ -1,6 +1,6 @@
 export const NAVIGATION_OPTIONS = [
   { icon: 'ic:baseline-home', label: 'Home' },
-  { icon: 'line-md:paint-drop-filled', label: 'Drop' },
+  { icon: 'mingcute:drop-fill', label: 'Drop' },
   { icon: 'mdi:account', label: 'Account' },
   { icon: 'icon-park-outline:application-menu', label: 'Menu' },
 ];
