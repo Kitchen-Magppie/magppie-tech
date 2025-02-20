680ee1fbe22af524a710b25e7e34783614912010
Akshay Kumar <akshay.official.dev@gmail.com>
2025-02-20 12:42:47 +0530

    added small changes

---
+++ app/pages/all-kitchens/all-kitchens.tsx
@@ -0,0 +1,10 @@
+import { MobileLayout } from '~/components';
+import { Header } from './components';
+
+export default function AllKitchens() {
+  return (
+    <MobileLayout>
+      <Header />
+    </MobileLayout>
+  );
+}
