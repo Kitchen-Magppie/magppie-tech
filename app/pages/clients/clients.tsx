1d389bfcb8d2e2fe3e3fb8c4ec51eca2aed857bc
Akshay Kumar <akshay.official.dev@gmail.com>
2025-02-22 00:20:37 +0530

    added small changes

--- app/pages/clients/clients.tsx
+++ app/pages/clients/clients.tsx
@@ -1,3 +1,20 @@
+import { MobileLayout } from '~/components';
+import { ClientsGrid } from './components';
+
 export default function Clients() {
-  return <></>;
+  return (
+    <MobileLayout>
+      <div className='px-4 pt-2 w-[280px]'>
+        <h1 className='text-[40px] x-font-dalglish-400 '>
+          where wellness meets trust
+        </h1>
+        <h2 className='text-[15px] x-fontbarlow-semi-condense-400 pt-6'>
+          Read stories of connection with our clients
+        </h2>
+      </div>
+      <div className='mb-10'>
+        <ClientsGrid />
+      </div>
+    </MobileLayout>
+  );
 }
