5aaf5ff15e245ba32a79e30615dd18dafe4b22b5
Akshay Kumar <akshay.official.dev@gmail.com>
2025-02-22 14:12:05 +0530

    Added small changes

---
+++ app/pages/profile/profile.tsx
@@ -0,0 +1,10 @@
+import { MobileLayout } from '~/components';
+import { UserCard } from './components';
+
+export default function Profile() {
+  return (
+    <MobileLayout>
+      <UserCard />
+    </MobileLayout>
+  );
+}
